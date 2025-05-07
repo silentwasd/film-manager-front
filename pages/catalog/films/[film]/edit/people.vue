38b3834765692f92646857892f722569e692f1b2
BashkirskyKot <silentwasd@gmail.com>
2025-05-07 05:59:50 +0300

    Make dubbing actor fill easier

--- pages/catalog/films/[film]/edit/people.vue
+++ pages/catalog/films/[film]/edit/people.vue
@@ -33,20 +33,37 @@ const activeRole = ref<PersonRole | null>(null);
                      @click="activeRole = role"/>
         </div>
 
-        <div class="flex flex-col gap-5">
-            <UiFilmPersonRow :film-id="film.id"
-                             :active-role="activeRole"
-                             @refresh="refreshPeople"/>
+        <div class="flex gap-5">
+            <div class="flex flex-col gap-5 grow"
+                 :class="{'overflow-auto h-[calc(100dvh-300px)]': activeRole == PersonRole.DubbingActor}">
+                <UiFilmPersonRow :film-id="film.id"
+                                 :active-role="activeRole"
+                                 @refresh="refreshPeople"/>
 
-            <UiFilmPersonRow v-for="person in (people?.data ?? []).filter(_person => activeRole ? _person.role == activeRole : true)"
-                             :key="person.id"
-                             :film-id="film.id"
-                             :person="person"
-                             @refresh="refreshPeople"/>
+                <UiFilmPersonRow
+                    v-for="person in (people?.data ?? []).filter(_person => activeRole ? _person.role == activeRole : true)"
+                    :key="person.id"
+                    :film-id="film.id"
+                    :person="person"
+                    @refresh="refreshPeople"/>
 
-            <UiFilmPersonRow :film-id="film.id"
-                             :active-role="activeRole"
-                             @refresh="refreshPeople"/>
+                <UiFilmPersonRow :film-id="film.id"
+                                 :active-role="activeRole"
+                                 @refresh="refreshPeople"/>
+            </div>
+
+            <div v-if="activeRole == PersonRole.DubbingActor"
+                 class="w-[300px] shrink-0 overflow-auto h-[calc(100dvh-300px)]">
+                <div class="flex flex-col gap-5">
+                    <div v-for="person in (people?.data ?? []).filter(_person => _person.role == PersonRole.Actor)"
+                         class="flex gap-2.5 items-center leading-5">
+                        <div class="w-16 h-16 bg-cover bg-center bg-no-repeat rounded-lg shrink-0 group overflow-clip"
+                             :style="`background-image: url(${person.person && person.person.photo ? fileUrl(person.person.photo) : '/img/person.jpg'});`"></div>
+
+                        <p>{{ person.role_details }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- types/enums/PersonRole.ts
+++ types/enums/PersonRole.ts
@@ -3,13 +3,13 @@ export enum PersonRole {
     Actor           = 'actor',
     VoiceActor      = 'voice-actor',
     Producer        = 'producer',
+    DubbingDirector = 'dubbing-director',
+    Translator      = 'translator',
+    DubbingActor    = 'dubbing-actor',
     Screenwriter    = 'screenwriter',
     Operator        = 'operator',
-    Artist          = 'artist',
-    Editor          = 'editor',
     Composer        = 'composer',
     SoundDirector   = 'sound-director',
-    DubbingDirector = 'dubbing-director',
-    DubbingActor    = 'dubbing-actor',
-    Translator      = 'translator'
+    Artist          = 'artist',
+    Editor          = 'editor'
 }
